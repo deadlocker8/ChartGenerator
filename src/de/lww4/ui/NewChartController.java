d4af1e7aed73941ff33fda2aab889c61ea77d819
deadlocker8 <deadlocker@gmx.de>
2016-11-16 16:33:05 +0100

    worked on TreeView in NewChartController

--- src/de/lww4/ui/NewChartController.java
+++ src/de/lww4/ui/NewChartController.java
@@ -1,14 +1,17 @@
 package de.lww4.ui;
 
+import java.util.ArrayList;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import de.lww4.main.CSVTable;
 import de.lww4.main.ChartType;
 import de.lww4.main.Dashboard;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ContentDisplay;
@@ -16,8 +19,8 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -35,7 +38,7 @@ public class NewChartController
 	@FXML private Button buttonSave;
 	@FXML private Button buttonCancel;
 	@FXML private HBox hboxChartTypes;
-	
+
 	public Stage stage;
 	private Controller controller;
 	public Image icon = new Image("de/lww4/resources/icon.png");
@@ -52,36 +55,38 @@ public class NewChartController
 		this.edit = edit;
 		this.dashboard = dashboard;
 		this.position = position;
-		
+
 		toggleGroupChartTypes = new ToggleGroup();
-		
+
 		for(ChartType currentType : ChartType.values())
 		{
 			RadioButton currentRadioButton = new RadioButton(currentType.getName());
 			currentRadioButton.setContentDisplay(ContentDisplay.BOTTOM);
 			currentRadioButton.setToggleGroup(toggleGroupChartTypes);
 			hboxChartTypes.getChildren().add(currentRadioButton);
-		}		
+		}
 		toggleGroupChartTypes.getToggles().get(0).setSelected(true);
 		toggleGroupChartTypes.selectedToggleProperty().addListener(new ChangeListener<Toggle>()
 		{
 			@Override
 			public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue)
 			{
-				generatePreview();				
+				generatePreview();
 			}
 		});
-		
+
 		colorPicker.setValue(Color.web("#508DC7"));
 		colorPicker.valueProperty().addListener(new ChangeListener<Color>()
 		{
 			@Override
 			public void changed(ObservableValue<? extends Color> observable, Color oldValue, Color newValue)
 			{
-				generatePreview();				
+				generatePreview();
 			}
 		});
-	
+
+		initTreeView();
+
 		if(edit)
 		{
 			// TODO get Chart from DB
@@ -89,15 +94,46 @@ public class NewChartController
 			// TODO fill form with existing data
 		}
 	}
-	
+
+	private void initTreeView()
+	{		
+		//DEBUG get from DB
+		ArrayList<CSVTable> tables = new ArrayList<>(); 		
+		
+		TreeItem<String> rootItem;
+		if(tables.size() == 0)
+		{
+			rootItem = new TreeItem<String>("Keine Daten verfügbar");					
+		}
+		else
+		{				
+			rootItem = new TreeItem<String>("CSV Tabellen");				
+		
+			for(CSVTable currentTable : tables)
+			{
+				TreeItem<String> currentMainItem = new TreeItem<String>(currentTable.getName());
+				
+				for(String currentColumn : currentTable.getColumnNames())
+				{
+					TreeItem<String> currentSubItem = new TreeItem<String>(currentColumn);
+					currentMainItem.getChildren().add(currentSubItem);
+				}						
+				
+				rootItem.getChildren().add(currentMainItem);
+			}
+		}
+		rootItem.setExpanded(true);
+		treeView.setRoot(rootItem);
+	}
+
 	private void generatePreview()
 	{
 		stackPaneChart.getChildren().clear();
-		//TODO generate chart
+		// TODO generate chart
 	}
 
 	public void save()
-	{	
+	{
 		String title = textFieldTitle.getText();
 		if(title == null || title.equals(""))
 		{
@@ -112,9 +148,9 @@ public class NewChartController
 			alert.showAndWait();
 			return;
 		}
-		
-		//TODO null check other values
-		//TODO save to DB
+
+		// TODO null check other values
+		// TODO save to DB
 	}
 
 	public void cancel()
