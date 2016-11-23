06b03911dcffe7c873c959cc6a29036b2bb0763c
Max Wittig <max.wittig95@gmail.com>
2016-11-23 17:19:46 +0100

    added CSV Tests

--- src/de/lww4/tests/importer/ValidCSVTest.java
+++ src/de/lww4/tests/importer/ValidCSVTest.java
@@ -1,10 +1,18 @@
 package de.lww4.tests.importer;
+import de.lww4.logic.DelimiterType;
+import de.lww4.logic.Importer;
+import java.io.File;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-public class ValidCSVTest extends AbstractCSVTests
+public class ValidCSVTest
 {
+
+    protected final File file = new File("src/de/lww4/tests/importer/data/valid.csv");
+    protected final String fillValue = "0";
+    protected final Importer importer = new Importer(file, DelimiterType.COMMA, fillValue, "valid.csv");
 
     /**
      * 4 columns exist
