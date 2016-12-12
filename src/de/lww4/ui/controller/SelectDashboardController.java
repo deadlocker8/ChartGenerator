f0d3f209ae3ed546bcc4e74a3e292ae2dc3e7930
Max Wittig <max.wittig95@gmail.com>
2016-12-12 12:59:45 +0100

    changed placeholder for select dashboard listview

--- src/de/lww4/ui/controller/SelectDashboardController.java
+++ src/de/lww4/ui/controller/SelectDashboardController.java
@@ -57,9 +57,9 @@ public class SelectDashboardController
 				}				
 			}
 		});
-		
-		Label labelPlaceholder = new Label("Keine Dashboards verfügbar.");
-		labelPlaceholder.setStyle("-fx-font-size: 14");
+
+        Label labelPlaceholder = new Label("Keine weiteren Dashboards verfügbar.");
+        labelPlaceholder.setStyle("-fx-font-size: 14");
 		listView.setPlaceholder(labelPlaceholder);
 		
 		refreshListView();			
