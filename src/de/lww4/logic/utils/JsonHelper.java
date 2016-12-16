ea894c54c081a8f65ca0eecb94d2801f4d1cb938
Robert Goldmann <deadlocker@gmx.de>
2016-12-16 18:13:41 +0100

    cleanup

--- src/de/lww4/logic/utils/JsonHelper.java
+++ src/de/lww4/logic/utils/JsonHelper.java
@@ -13,32 +13,23 @@ import java.util.Map;
 
 public class JsonHelper
 {
-    public static HashMap<Double, String> getScaleHashMapFromJson(String jsonContent)
-    {
-        HashMap<Double, String> scaleHashMap = new HashMap<>();
-        JsonParser jsonParser = new JsonParser();
-        JsonElement root = jsonParser.parse(jsonContent);
+	@SuppressWarnings("rawtypes")
+	public static HashMap<Double, String> getScaleHashMapFromJson(String jsonContent)
+	{
+		HashMap<Double, String> scaleHashMap = new HashMap<>();
+		JsonParser jsonParser = new JsonParser();
+		JsonElement root = jsonParser.parse(jsonContent);
 
-        for(Map.Entry entry : root.getAsJsonObject().entrySet())
-        {
-            scaleHashMap.put(Double.valueOf(entry.getKey().toString()), entry.getValue().toString());
-        }
-        return scaleHashMap;
-    }
+		for(Map.Entry entry : root.getAsJsonObject().entrySet())
+		{
+			scaleHashMap.put(Double.valueOf(entry.getKey().toString()), entry.getValue().toString());
+		}
+		return scaleHashMap;
+	}
 
-    public static String convertScaleHashMapToJson(HashMap<Double, String> hashMap)
-    {
-        Gson gson = new Gson();
-        return gson.toJson(hashMap);
-    }
-
-    public static void main(String args[])
-    {
-        HashMap<Double, String> hashMap = new HashMap<>();
-        hashMap.put(12.5, "Test");
-        hashMap.put(142.5, "dsf");
-        String hashMapString = convertScaleHashMapToJson(hashMap);
-        System.out.println(hashMapString);
-        System.out.println(getScaleHashMapFromJson(hashMapString));
-    }
+	public static String convertScaleHashMapToJson(HashMap<Double, String> hashMap)
+	{
+		Gson gson = new Gson();
+		return gson.toJson(hashMap);
+	}
 }
